4c3bfe49ba676c652d7bbd82042f42acff9e7e85
Jeppe Hansen <jeppehansen016@gmail.com>
2026-04-30 11:41:51 +0200

    Ændringer til createDB

--- db/createDb.js
+++ db/createDb.js
@@ -6,5 +6,35 @@ const timestamp = (await db.query("select now() as timestamp")).rows[0][
 ];
 console.log(`Recreating database on ${timestamp}...`);
 
+// genre
+// artist
+// tracks
+// session
+// user
+// votes
+
+await db.query(`
+    create table genre (
+        genre_id   integer primary key,
+        genre text,
+    )
+`);
+
+await db.query(`
+    create table artist (
+        artist_id   integer primary key,
+        artist text,
+    )
+`);
+
+await db.query(`
+    create table tracks (
+        track_id   integer primary key,
+        songname text,
+        artist_id integer references artist
+        genre_id integer references genre
+    )
+`);
+
 await db.end();
 console.log("Database successfully recreated. KOM NUUUUU!!!!");
