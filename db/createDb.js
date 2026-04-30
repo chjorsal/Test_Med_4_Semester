8075029482d8ea471994c6fcb753927acba08cdd
Jeppe Hansen <jeppehansen016@gmail.com>
2026-04-30 12:50:44 +0200

    Lavet database
    Lavet database

--- db/createDb.js
+++ db/createDb.js
@@ -6,24 +6,24 @@ const timestamp = (await db.query("select now() as timestamp")).rows[0][
 ];
 console.log(`Recreating database on ${timestamp}...`);
 
-// genre
-// artist
-// tracks
-// session
-// user
-// votes
+await db.query("drop table if exists votes");
+await db.query("drop table if exists tracks");
+await db.query("drop table if exists artist");
+await db.query("drop table if exists users");
+await db.query("drop table if exists session");
+await db.query("drop table if exists genre");
 
 await db.query(`
     create table genre (
         genre_id   integer primary key,
-        genre text,
+        genre text
     )
 `);
 
 await db.query(`
     create table artist (
         artist_id   integer primary key,
-        artist text,
+        artist text
     )
 `);
 
@@ -31,8 +31,30 @@ await db.query(`
     create table tracks (
         track_id   integer primary key,
         songname text,
-        artist_id integer references artist
+        artist_id integer references artist,
         genre_id integer references genre
+    )
+`);
+
+await db.query(`
+    create table session (
+        session_id   integer primary key,
+        genre_id integer references genre
+    )
+`);
+
+await db.query(`
+    create table users (
+        user_id   integer primary key,
+        name text
+    )
+`);
+
+await db.query(`
+    create table votes (
+        session_id integer references session,
+        user_id integer references users,
+        track_id integer references tracks
     )
 `);
 
