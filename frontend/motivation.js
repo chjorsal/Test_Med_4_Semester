26306ae5df2ee36f80320d7c53b5cd16b1407bbc
Jeppe Hansen <jeppehansen016@gmail.com>
2026-05-05 13:27:18 +0200

    Lavet så der kan komme en tilfældig sang op

--- frontend/motivation.js
+++ frontend/motivation.js
@@ -1,4 +1,5 @@
 const element = document.getElementById("content");
+
 const params = new URLSearchParams(window.location.search);
 const sessionId = params.get("id");
 
@@ -9,7 +10,16 @@ async function loadAndRenderSuggestions(sessionId, element) {
   if (response.ok) {
     const suggestions = await response.json();
     console.log(suggestions);
+    const tracksElement = document.createElement("div");
+    renderTrack(suggestions, tracksElement);
+    element.appendChild(tracksElement);
   } else {
     element.textContent = "Could not get suggestions, try again later";
   }
+}
+
+function renderTrack(suggestions, element) {
+  const tracksElement = document.createElement("h1");
+  tracksElement.textContent = ` ${suggestions[0].songname}`;
+  element.appendChild(tracksElement);
 }
