63e3603650f8fe0506a1ef46d255c0196604dab1
chjorsal <ch.jorsal@gmail.com>
2026-05-07 12:20:29 +0200

    Made a delete funktion
    Made the delete funktion so votes last one round

--- frontend/votes.js
+++ frontend/votes.js
@@ -4,7 +4,7 @@ async function loadVotes() {
 
   votes.forEach(function (song) {
     const btn = document.querySelector(
-      'button[data-track-id="' + song.track_id + '"]' // id er de forskellige knapper
+      'button[data-track-id="' + song.track_id + '"]', // id er de forskellige knapper
     );
 
     if (btn) {
@@ -15,33 +15,49 @@ async function loadVotes() {
 }
 
 async function vote(btn) {
-  if (btn.disabled) { //
+  if (btn.disabled) {
+    //
     return;
   }
 
   const track_Id = btn.dataset.trackId;
 
-  const response = await fetch("/api/votes" , {// poster et count på vores server.js og i vores SQL
+  const response = await fetch("/api/votes", {
+    // poster et count på vores server.js og i vores SQL
     method: "POST",
     headers: {
-    "Content-Type": "application/json"
-  },
-    body : JSON.stringify ({track_id: track_Id, user_id: 1, session_id: 1})  // de skal laves om senere så de ikke er fixed
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ track_id: track_Id, user_id: 1, session_id: 1 }), // de skal laves om senere så de ikke er fixed
     // de skal astates af currentSessionId eller ligende
   });
 
- if (!response.ok) { // hvis dette reponse ikke er ok så slå den knappen fra stem og i stedet 
-  console.error("Server error:", response.status, await response.text());
+  if (!response.ok) {
+    // hvis dette reponse ikke er ok så slå den knappen fra stem og i stedet
+    console.error("Server error:", response.status, await response.text());
+    btn.disabled = true;
+    btn.textContent = "Har stemt";
+    return;
+  }
+
+  const countEl = btn.nextElementSibling;
+  countEl.textContent = parseInt(countEl.textContent) + 1;
   btn.disabled = true;
   btn.textContent = "Har stemt";
-  return;
-}
-
-const countEl = btn.nextElementSibling;
-countEl.textContent = parseInt(countEl.textContent) + 1;
-btn.disabled = true;
-btn.textContent = "Har stemt";
-
 }
 
 loadVotes();
+
+// denne funktion kører i et specifikt tidsinterval og nulstiller stemmerne og gør det muligt at stemme
+// igen på en sang.
+setInterval(async function () {
+  await fetch("/api/votes", {
+    method: "DELETE",
+  });
+  await loadVotes();
+
+  document.querySelectorAll("button[data-track-id]").forEach(function (btn) {
+    btn.disabled = false;
+    btn.textContent = "Stem";
+  });
+}, 10000);
